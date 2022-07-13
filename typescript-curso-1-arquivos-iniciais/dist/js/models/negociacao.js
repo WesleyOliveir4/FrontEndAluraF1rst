74a035c9363b7a977c8f07eb4ee890f353c9bed5
wesley oliveira <wlwwesley9@gmail.com>
2022-07-12 23:35:43 -0300

    O modificador private

--- typescript-curso-1-arquivos-iniciais/dist/js/models/negociacao.js
+++ typescript-curso-1-arquivos-iniciais/dist/js/models/negociacao.js
@@ -1,37 +1,19 @@
-var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, privateMap, value) {
-    if (!privateMap.has(receiver)) {
-        throw new TypeError("attempted to set private field on non-instance");
-    }
-    privateMap.set(receiver, value);
-    return value;
-};
-var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, privateMap) {
-    if (!privateMap.has(receiver)) {
-        throw new TypeError("attempted to get private field on non-instance");
-    }
-    return privateMap.get(receiver);
-};
-var _data, _quantidade, _valor;
 export class Negociacao {
     constructor(data, quantidade, valor) {
-        _data.set(this, void 0);
-        _quantidade.set(this, void 0);
-        _valor.set(this, void 0);
-        __classPrivateFieldSet(this, _data, data);
-        __classPrivateFieldSet(this, _quantidade, quantidade);
-        __classPrivateFieldSet(this, _valor, valor);
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
     }
-    get () {
-        return __classPrivateFieldGet(this, _data);
+    get data() {
+        return this._data;
     }
-    get () {
-        return __classPrivateFieldGet(this, _quantidade);
+    get quantidade() {
+        return this._quantidade;
     }
-    get () {
-        return __classPrivateFieldGet(this, _valor);
+    get valor() {
+        return this._valor;
     }
-    get () {
-        return __classPrivateFieldGet(this, _valor) * __classPrivateFieldGet(this, _quantidade);
+    get volume() {
+        return this.valor * this.quantidade;
     }
 }
-_data = new WeakMap(), _quantidade = new WeakMap(), _valor = new WeakMap();
